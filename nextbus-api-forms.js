205ea904c5cc568d7cf948c7c65a801e77a45e96
Hack Reactor Students <github@hackreactor.com>
2013-08-01 21:14:29 -0700

    Refactored files, fixed bugs.
    Moved D3 to separate js file.
    
    Fixed inbound/outbound corresponding stops bug.

--- nextbus-api-forms.js
+++ nextbus-api-forms.js
@@ -37,10 +37,10 @@ var parseXMLstops = function(xml, cb) {
     };
   });
 
-  cb(stopsInfo, routes, routeTag);
+  cb(stopsInfo, routes);
 };
 
-var displayRoutes = function(stopsInfo, routes, dirTag) {
+var displayRoutes = function(stopsInfo, routes) {
   var $directionSel = $("#directionSelector");
   $directionSel.empty();
   $("#stopSelector").empty();
@@ -52,21 +52,21 @@ var displayRoutes = function(stopsInfo, routes, dirTag) {
     $directionSel.append('<option value="'+ key +'">'+ route.title + opt1 +'</option>');
   });
 
+  var dirTag = $("#directionSelector").val();
+
   displayStops(stopsInfo, routes, dirTag);
 };
 
-var displayStops = function(stopsInfo, routes, dirTag, selectedStop) {
+var displayStops = function(stopsInfo, routes, dirTag) {
   var $stopSel = $("#stopSelector");
   $stopSel.empty();
-
-  selectedStop = selectedStop || routes[dirTag].stops[0];
 
   _(routes[dirTag].stops).each(function(stoppy){
     $stopSel.append('<option value="'+ stoppy +'">'+ stopsInfo[stoppy].title +'</option>');
   });
 };
 
-var getStopTimes = function(stopTag, routeTag, cb, svgElement, routeTitle){
+var getStopTimes = function(stopTag, routeTag, cb, svgElement){
   var routeQuery = '&r=' + routeTag;
   var URLpredictions = 'http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&s=' + stopTag + routeQuery;
   var times = [];
@@ -76,7 +76,7 @@ var getStopTimes = function(stopTag, routeTag, cb, svgElement, routeTitle){
       times.push($(this).attr('seconds'));
     });
 
-    cb(times, svgElement, routeTitle);
+    cb(times, svgElement);
   }, 'xml');
 };
 
---
+++ nextbus-d3.js
@@ -0,0 +1,125 @@
+  // makeChart inserts a DIV and SVG within the DIV, returns a chart obj
+  //  that keeps track of various items related to the chart
+var makeChart = function(stop, route) {
+  var chart = {};
+
+  $(".chartsDiv").prepend('<div class="chart_class"></div>');
+  chart.div = $(".chartsDiv").children().first();
+
+  $(chart.div).html('<div class="route_title">'+ routesList[route] + '</div>');
+
+  chart.vis = d3.select(".chart_class:first-child").append("svg:svg")
+              .attr('width', w)
+              .attr('height', h)
+              .style('border', '1px solid black');
+
+  getStopTimes(stop, route, render, chart.vis);
+  chart.timer = setInterval(function(){
+    getStopTimes(stop,route, render, chart.vis);
+  }, 12000);
+
+  return chart;
+};
+
+var updateChart = function(stop, route, chart) {
+  window.clearInterval(chart.timer);
+  $(chart.vis[0]).empty();
+  $(chart.div).children().first().text(routesList[route]);
+
+  getStopTimes(stop, route, render, chart.vis);
+  chart.timer = setInterval(function(){
+    getStopTimes(stop,route, render, chart.vis);
+  }, 12000);
+};
+
+
+var w = 900,
+    h = 500;
+
+var xPad = 10,
+    yPad = 10,
+    barPad = 1;
+
+var divisor = 5;
+
+var xScale = d3.scale.linear()
+                .domain([])
+                .range([xPad,w-xPad]);
+
+var render = function(dataset, vis) {
+  var bar = vis.selectAll('rect');
+  var minTxt = vis.selectAll("text");
+
+  var skipFlag = false;
+  var tr_time = 10500;
+
+    // checking if the bus rolls off the stop
+    //    condition: if the last reported time was <30s and the new time is 90s greater
+  if(bar[0] && bar[0][0]) {
+    var pWidth = parseInt( d3.select(bar[0][0]).attr("width") )*divisor;
+    if( (pWidth < 30) && ( parseInt(dataset[0]) - pWidth ) > 90 ) {
+      skipFlag = true;
+      tr_time = 5000;
+      bar[0][0].remove();
+      minTxt[0][0].remove();
+      bar[0].splice(0,1);
+      minTxt[0].splice(0,1);
+    } else {
+      skipFlag = false;
+    }
+  }
+
+  var barProperties = function(obj){
+    obj.attr("y", function(d, i) {
+          return i * (h / (dataset.length)) + yPad;
+        })
+        .attr("width", function(d) {
+          return Math.floor(d/divisor);
+        })
+        .attr("height", (h - 2*yPad)/dataset.length - barPad)
+        .attr("fill", function(d) {
+          var g = Math.floor((1 - d/5000)*255);
+          return "rgb(0, "+ g +", 0)";
+        });
+  };
+
+  var labelProperties = function(obj) {
+    obj.text(function(d) {
+          var minutes = Math.floor(d/60);
+          return minutes + ' min';
+        })
+        .attr("x", function(d) {
+          var fromEdge = 20;
+          if(Math.floor(d/60)>9) {
+            fromEdge = 27;
+          }
+          return Math.floor(d/divisor) - fromEdge;
+        })
+        .attr("y", function(d, i) {
+          return i * (h / (dataset.length)) + yPad + 10;
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", "white");
+  };
+
+  bar = bar.data(dataset);
+
+  var upBar = bar.transition().duration(tr_time);
+  barProperties(upBar);
+
+  var newBar = bar.enter().append('rect').attr("x", xPad);
+  barProperties(newBar);
+
+  bar.exit().remove();
+
+  minTxt = minTxt.data(dataset);
+
+  var upMin = minTxt.transition().duration(tr_time);
+  labelProperties(upMin);
+
+  var newMin = minTxt.enter().append("text");
+  labelProperties(newMin);
+
+  minTxt.exit().remove();
+};
