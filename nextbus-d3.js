99455d21c8e534216ad0ff8779ebc21490357208
Hack Reactor Students <github@hackreactor.com>
2013-08-06 12:08:31 -0700

    WIP - arcs implemented, but won't animate. Need to work on tweens?

--- nextbus-d3.js
+++ nextbus-d3.js
@@ -17,15 +17,6 @@ var makeChart = function(stop, route) {
               .attr('height', h)
               .style('border', '1px solid black');
 
-  var $svg = $(chart.vis[0]);
-
-  $svg.click(function(){
-    var state1 = 'border: 1px solid black';
-    var state2 = 'border: 2px solid red';
-
-    $svg.attr("style", ($svg.attr("style")===state1) ? state2 : state1 );
-  });
-
   updateChart(stop, route, chart);
 
   return chart;
@@ -78,56 +69,52 @@ var updateChart = function(stop, route, chart) {
     return times;
   };
 
-  getNextbus(query0, function(xml) {
-    var stop0 = parseXMLtimes(xml);
-    getNextbus(query1, function(xml2){
-      var stop1 = parseXMLtimes(xml2);
-      var times = syncPredictions(stop0, stop1);
+  // getNextbus(query0, function(xml) {
+  //   var stop0 = parseXMLtimes(xml);
+  //   getNextbus(query1, function(xml2){
+  //     var stop1 = parseXMLtimes(xml2);
+  //     var times = syncPredictions(stop0, stop1);
 
-      render(times, chart.vis);
-    });
+  //     render(times, chart.vis);
+  //   });
+  // });
+
+  // chart.timer = setInterval(function(){
+  //   getNextbus(query0, function(xml) {
+  //     var stop0 = parseXMLtimes(xml);
+  //     getNextbus(query1, function(xml2){
+  //       var stop1 = parseXMLtimes(xml2);
+  //       var times = syncPredictions(stop0, stop1);
+
+  //       render(times, chart.vis);
+  //     });
+  //   });
+  // }, 12000);
+
+  getNextbus(query0, function(xml){
+    render(parseXMLtimes(xml), chart.vis);
   });
 
-  chart.timer = setInterval(function(){
-    getNextbus(query0, function(xml) {
-      var stop0 = parseXMLtimes(xml);
-      getNextbus(query1, function(xml2){
-        var stop1 = parseXMLtimes(xml2);
-        var times = syncPredictions(stop0, stop1);
-
-        render(times, chart.vis);
-      });
+  chart.time = setInterval(function(){
+    getNextbus(query0, function(xml){
+      render(parseXMLtimes(xml), chart.vis);
     });
   }, 12000);
 };
 
 var render = function(dataset, vis) {
-  console.log("Incoming buses: ", dataset[0]);
+  console.log("Incoming buses: ", dataset);
 
-  var layer = vis.selectAll(".layer")
-    .data(dataset)
-    .enter().append("g")
-    .attr("class", "layer");
-
-  var bar = layer.selectAll('rect');
-  var minTxt = layer.selectAll("text");
-
-  var xPad = 10,
-      yPad = 10,
-      barPad = 1;
-
-  var divisor = 5;
-
-  var xScale = d3.scale.linear()
-                  .domain([])
-                  .range([xPad,w-xPad]);
+  var g = vis.selectAll("g")
+      .data(dataset)
+      .enter().append("g");
 
   var skipFlag = false;
   var tr_time = 10500;
 
     // checking if the bus rolls off the stop
     //    condition: if the last reported time was <30s and the new time is 90s greater
-  if(bar[0] && bar[0][0]) {
+/*  if(bar[0] && bar[0][0]) {
     var pWidth = parseInt( d3.select(bar[0][0]).attr("width") )*divisor;
     if( (pWidth < 30) && ( parseInt(dataset[0][0].seconds) - pWidth ) > 90 ) {
       skipFlag = true;
@@ -140,95 +127,47 @@ var render = function(dataset, vis) {
       skipFlag = false;
     }
   }
+*/
 
-  var barProperties = function(obj){
-    obj.attr("x", function(d) {
-          if(d.offset) {
-            return Math.floor(d.offset/divisor) + xPad + 3;
-          } else {
-            return xPad;
-          }
-        })
-        .attr("y", function(d, i) {
-          return i * ((h - 2*yPad) / (dataset[0].length)) + yPad;
-        })
-        .attr("width", function(d) {
-          return Math.floor(d.seconds/divisor);
-        })
-        .attr("height", (h - 2*yPad)/dataset[0].length - barPad)
-        .attr("fill", function(d) {
-          if(d.offset) {
-            return "rgb(255, 255, 0)";
-          } else {
-            var g = Math.floor((1 - d.seconds/5000)*255);
+  var pi = Math.PI;
+  var aMin = 75;
+  var aWidth = 15;
+  var aPad = 1;
+
+  var arc = d3.svg.arc()
+      .innerRadius(function(d, i) {
+        return aMin + i*(aWidth) + aPad;
+      })
+      .outerRadius(function(d, i) {
+        return aMin + (i+1)*(aWidth);
+      })
+      .startAngle(0 * (pi/180))
+      .endAngle(function(d) {
+        return Math.ceil(d.seconds/60)*6 * (pi/180);
+      });
+
+  g.append("path")
+      .attr("d", arc)
+      .attr("transform", 'translate('+ w/2 +','+ h/2 +')')
+      .attr("fill", function(d){
+        var g = Math.floor((1 - d.seconds/4000)*255);
+        return "rgb(0, "+ g +", 0)";
+      })
+      .on("click", function(d, i){
+        d3.select(this).attr("fill", function(d){
+          var thisArc = d3.select(this);
+          if(thisArc.attr("fill")==='red'){
+            var g = Math.floor((1 - d.seconds/4000)*255);
             return "rgb(0, "+ g +", 0)";
+          } else {
+            return 'red';
           }
         });
-  };
+        console.log(i, ': ', d.seconds);
+      });
 
-  var labelProperties = function(obj) {
-    obj.text(function(d) {
-          var min = Math.floor(d.seconds/60);
-          if(d.offset) {
-            var offset = Math.floor(d.offset/60);
-            min = Math.ceil(d.seconds/60);
-            if(min>5) {
-              var total = parseInt(min) + parseInt(offset);
-              return '+'+ min +' = '+ total +' min';
-            } else {
-              return '+'+ min;
-            }
-          } else {
-            var minutes = Math.floor(d.seconds/60);
-            return min + ' min';
-          }
-        })
-        .attr("x", function(d) {
-          var fromEdge = (Math.floor(d.seconds/60)>9) ? 27 : 20;
-          if(d.offset) {
-            var offset = Math.floor(d.offset/60);
-            var total = parseInt(d.seconds) + parseInt(d.offset);
-            if( Math.ceil(d.seconds/60) > 5 ) {
-              fromEdge += (total>9) ? 27 : 20;
-            } else {
-              fromEdge = 1;
-            }
-            return Math.floor(total/divisor) - fromEdge;
-          } else {
-            return Math.floor(d.seconds/divisor) - fromEdge;
-          }
-        })
-        .attr("y", function(d, i) {
-          return i * ((h - 2*yPad) / (dataset[0].length)) + yPad + 10;
-        })
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "11px")
-        .attr("fill", function(d) {
-          return (d.offset) ? 'black' : 'white';
-        });
-  };
+      // can't get it to animate
+  g.select("path")
+    .attr("d", arc);
 
-  bar = bar.data(function(d){
-    return d;
-  });
-
-  var upBar = bar.transition().duration(tr_time);
-  barProperties(upBar);
-
-  var newBar = bar.enter().append('rect').attr("x", xPad);
-  barProperties(newBar);
-
-  bar.exit().remove();
-
-  minTxt = minTxt.data(function(d){
-    return d;
-  });
-
-  var upMin = minTxt.transition().duration(tr_time);
-  labelProperties(upMin);
-
-  var newMin = minTxt.enter().append("text");
-  labelProperties(newMin);
-
-  minTxt.exit().remove();
 };
