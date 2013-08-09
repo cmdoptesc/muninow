59495687422fcf4c849d58126fccd51f55501072
Hack Reactor Students <github@hackreactor.com>
2013-08-08 23:03:50 -0700

    WIP - Have quickFlash animation to ripple for initialization.
    Need to have graphic revert back to original state when done.

--- js/mymuni-d3.js
+++ js/mymuni-d3.js
@@ -52,7 +52,7 @@ var updateChart = function(stop, route, chart) {
 
   var parseAndRender = function(xml) {
     var times = getSixSoonest(parseXMLmulti(xml));
-    render(times, chart.vis);
+    d3methods.render(times, chart.vis);
   };
 
   if(chart.timer) { window.clearInterval(chart.timer); }
@@ -69,120 +69,158 @@ var updateChart = function(stop, route, chart) {
   }, 14500);
 };
 
-var render = function(dataset, vis) {
 
-  // constants
-  var w = $(vis[0]).width();
-  var h = $(vis[0]).height();
-  var smlr = (w<h) ? w : h;
+var d3methods = {
 
-  var cX = Math.round(w/2);
-  var cY = ((h/2) > w) ? Math.floor(h/3) : Math.floor(h/2);
+  _selectionColor: 'rgb(252,125,71)',
+  _highlightColor: 'rgb(222,174,138)',
 
-  var pi = Math.PI;
-  var arcMin = Math.floor(smlr*0.15)  ;
-  var arcWidth = Math.floor(arcMin/3.75);
-  var arcPad = Math.ceil(arcWidth*0.1);
-  var selectionColor = 'rgb(252,125,71)';
-
-  var colorScale = d3.scale.linear()
-      .domain([0, d3.max(dataset, function(d) { return parseInt(d.seconds, 10); })])
-      .range(["rgb(242,229,211)","rgb(191,223,205)","rgb(139,206,180)"]);
-
-  var transitionTime = 3000;
-
-    // main group where objects are located -- saves from declaring multiple transforms
-  var g = vis.select("g.centerGroup");
-  g.attr("transform", 'translate('+ cX +','+ cY +')');
-
-  var gArc = g.selectAll("g.arcGroup");
-  var d3centerText = g.selectAll("#timeDisplay");
-
-    // checks to see if the past bus has rolled off, if so, delete the associated graphic
-  if(gArc[0] && gArc[0][0]) {
-    var pastBus = d3.select(gArc[0][0]).select("path.arcPath").datum();
-    if( (pastBus.seconds<45) && (dataset[0].vehicle != pastBus.vehicle) ) {
-      transitionTime = 1000;
-      gArc[0][0].remove();
-      gArc[0].splice(0,1);
-    }
-  }
-
-  gArc = gArc.data(dataset);
-  var centerTextData = [dataset[0]];
-
-  var updateCenter = function(newData) {
-    d3centerText.data(newData).text(function(d){
-      return toMin(d.seconds);
-    })
-    .style("font-size", Math.floor(arcMin*1.44) + 'px')
-    .attr("transform", 'translate(0,'+ parseInt(arcMin/2, 10) +')');
-  };
-
-  var toMin = function(sec) {
+  _toMin: function(sec) {
     var fuzzy = 5*( 5 - sec/60 );
     return ( sec%60 > fuzzy ) ? Math.ceil(sec/60) : Math.floor(sec/60);
-  };
+  },
 
-    // defining arc accessor
-  var arc = d3.svg.arc()
-      .innerRadius(function(d, i) {
-        return arcMin + i*(arcWidth) + arcPad;
+  _colorScaleMaker: function(max) {
+    return d3.scale.linear()
+        .domain([0, max])
+        .range(["rgb(242,229,211)","rgb(191,223,205)","rgb(139,206,180)"]);
+  },
+
+  quickFlash: function(vis) {
+    var d3arcs = vis.selectAll("path.arcPath");
+    var d3centerText = vis.selectAll("#timeDisplay");
+
+    var lastIndex = d3arcs[0].length-1;
+
+    var colorScale = d3methods._colorScaleMaker( d3.select(d3arcs[0][lastIndex]).datum().seconds );
+    var highlightColor = d3methods._highlightColor;
+
+    var arcs = vis.selectAll("path.arcPath")
+        .transition()
+          .delay(function(d, i){
+            return i*350;
+          })
+          .duration(800)
+          .attr("fill", highlightColor)
+          .each("end", function(d, i){
+            d3centerText.text(d3methods._toMin(d.seconds));
+            d3.select(this)
+              .transition()
+              .duration(300)
+              .attr("fill", colorScale(d.seconds));
+          });
+  },
+
+
+  render: function(dataset, vis) {
+    // constants
+    var w = $(vis[0]).width();
+    var h = $(vis[0]).height();
+    var smlr = (w<h) ? w : h;
+
+    var cX = Math.round(w/2);
+    var cY = ((h/2) > w) ? Math.floor(h/3) : Math.floor(h/2);
+
+    var pi = Math.PI;
+    var arcMin = Math.floor(smlr*0.15);
+    var arcWidth = Math.floor(arcMin/3.75);
+    var arcPad = Math.ceil(arcWidth*0.1);
+    var selectionColor = d3methods._selectionColor;
+
+    var colorScale = d3methods._colorScaleMaker( d3.max(dataset, function(d) {
+      return parseInt(d.seconds, 10);
+    }) );
+
+    var transitionTime = 3000;
+
+      // main group where objects are located -- saves from declaring multiple transforms
+    var g = vis.select("g.centerGroup");
+    g.attr("transform", 'translate('+ cX +','+ cY +')');
+
+    var gArc = g.selectAll("g.arcGroup");
+    var d3centerText = g.selectAll("#timeDisplay");
+
+      // checks to see if the past bus has rolled off, if so, delete the associated graphic
+    if(gArc[0] && gArc[0][0]) {
+      var pastBus = d3.select(gArc[0][0]).select("path.arcPath").datum();
+      if( (pastBus.seconds<45) && (dataset[0].vehicle != pastBus.vehicle) ) {
+        transitionTime = 1000;
+        gArc[0][0].remove();
+        gArc[0].splice(0,1);
+      }
+    }
+
+    gArc = gArc.data(dataset);
+    var centerTextData = [dataset[0]];
+
+    var updateCenter = function(newData) {
+      d3centerText.data(newData).text(function(d){
+        return d3methods._toMin(d.seconds);
       })
-      .outerRadius(function(d, i) {
-        return arcMin + (i+1)*(arcWidth);
-      })
-      .startAngle(0 * (pi/180))
-      .endAngle(function(d) {
-        return parseFloat((d.seconds/60)*6 * (pi/180));
-      });
+      .style("font-size", Math.floor(arcMin*1.44) + 'px')
+      .attr("transform", 'translate(0,'+ parseInt(arcMin/2, 10) +')');
+    };
 
-    // update for arcs
-  gArc.select("path.arcPath")
-      .transition()
-      .duration(transitionTime)
-      .attr("fill", function(d){
-        if(d3.select(this).attr("fill")===selectionColor) {
-          centerTextData = [d3.select(this).datum()];
-          updateCenter(centerTextData);
-          return selectionColor;
-        } else {
-          return colorScale(d.seconds);
-        }
-      })
-      .attr("d", arc);
-
-    // enter for arcs
-  gArc.enter().append("svg:g").attr("class", 'arcGroup')
-      .append("svg:path")
-      .attr("class", 'arcPath')
-      .attr("fill", function(d, i){
-        return (i===0) ? selectionColor: colorScale(d.seconds);
-      })
-      .attr("d", arc);
-
-    // moved event handler from enter() as there were closure issues with referencing old dataset[0]
-  d3.selectAll("path.arcPath")
-    .on("click", function(d, i){
-        var d3selected = d3.select(this);
-
-        _(d3.selectAll("path")[0]).each(function(arcPath){
-          var d3arc = d3.select(arcPath);
-          d3arc.attr("fill", colorScale(d3arc.datum().seconds));
+      // defining arc accessor
+    var arc = d3.svg.arc()
+        .innerRadius(function(d, i) {
+          return arcMin + i*(arcWidth) + arcPad;
+        })
+        .outerRadius(function(d, i) {
+          return arcMin + (i+1)*(arcWidth);
+        })
+        .startAngle(0 * (pi/180))
+        .endAngle(function(d) {
+          return Math.round(parseFloat((d.seconds/60)*6 * (pi/180)) * 10000, 10)/10000;
         });
 
-        d3selected.attr("fill", selectionColor);
-        centerTextData = [d];
-        $('.route_title').text(routesList[centerTextData[0].routeTag]);
-        updateCenter(centerTextData);
+      // update for arcs
+    gArc.select("path.arcPath")
+        .transition()
+        .duration(transitionTime)
+        .attr("fill", function(d){
+          if(d3.select(this).attr("fill")===selectionColor) {
+            centerTextData = [d3.select(this).datum()];
+            updateCenter(centerTextData);
+            return selectionColor;
+          } else {
+            return colorScale(d.seconds);
+          }
+        })
+        .attr("d", arc);
 
-        //console.log(d.routeTag +': '+ d.seconds);
-      });
+      // enter for arcs
+    gArc.enter().append("svg:g").attr("class", 'arcGroup')
+        .append("svg:path")
+        .attr("class", 'arcPath')
+        .attr("fill", function(d, i){
+          return (i===0) ? selectionColor: colorScale(d.seconds);
+        })
+        .attr("d", arc);
 
-    // enter and update for the center text
-  d3centerText = d3centerText.data(centerTextData);
-  d3centerText.enter().append("svg:text")
-      .attr("id", "timeDisplay")
-      .attr("text-anchor", 'middle');
-  updateCenter(centerTextData);
+      // moved event handler from enter() as there were closure issues with referencing old dataset[0]
+    d3.selectAll("path.arcPath")
+      .on("click", function(d, i){
+          var d3selected = d3.select(this);
+
+          _(d3.selectAll("path")[0]).each(function(arcPath){
+            var d3arc = d3.select(arcPath);
+            d3arc.attr("fill", colorScale(d3arc.datum().seconds));
+          });
+
+          d3selected.attr("fill", selectionColor);
+          centerTextData = [d];
+          $('.route_title').text(routesList[centerTextData[0].routeTag]);
+          updateCenter(centerTextData);
+
+          //console.log(d.routeTag +': '+ d.seconds);
+        });
+
+      // enter and update for the center text
+    d3centerText = d3centerText.data(centerTextData);
+    d3centerText.enter().append("svg:text")
+        .attr("id", "timeDisplay")
+        .attr("text-anchor", 'middle');
+    updateCenter(centerTextData);
+  }
 };
